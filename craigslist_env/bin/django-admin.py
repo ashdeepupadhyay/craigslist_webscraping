6a52d875777016d5da0788166970a67e6f7cdbc2
Ashdeep <ashdeep@INLaptop174.local>
2020-03-01 15:28:31 +0530

    adding the virtual environment

---
+++ craigslist_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ashdeep/ashdeeplearn/projects/python_django/craigslist_webscraping/craigslist_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
