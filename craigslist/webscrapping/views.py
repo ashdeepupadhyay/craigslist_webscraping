28b6fb6f82cd37bd03e31e9a8a5980702d683cfd
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2020-03-02 22:47:44 +0530

    install requests and fetch HTML data using requests

--- craigslist/webscrapping/views.py
+++ craigslist/webscrapping/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+import requests
+from requests.compat import quote_plus
+
 
 # Create your views here.
+BASE_CRAIGS_LIST_URL="https://delhi.craigslist.org/search/?query={}"
 def home(request):
     return render(request,'base.html')
 
 def new_search(request):
     search=request.POST.get('search')
-    print(search)
+    #print(quote_plus(search))
+    final_url=BASE_CRAIGS_LIST_URL.format(quote_plus(search))
+    print(final_url)
+    #response=requests.get('https://delhi.craigslist.org/search/bbb?query=python&sort=rel')
+    response = requests.get(final_url)
+    data=response.text
+    print(data)
+    #print(search)
     stuff_for_frontend={
         'search':search
     }
