48c55144d0df84fdaa44c15d97c68967c204763b
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2020-03-04 09:25:53 +0530

    testing the data in the front end

--- craigslist/webscrapping/views.py
+++ craigslist/webscrapping/views.py
@@ -21,14 +21,23 @@ def new_search(request):
     data=response.text
     soup = BeautifulSoup(data,features='html.parser')
     post_listings=soup.find_all('li',{'class':'result-row'})
-    print(len(post_listings))
-    post_title=post_listings[0].find(class_='result-title').text
-    post_url=post_listings[0].find('a').get('href')
-    post_price=post_listings[0].find(class_='result-price').text
+    #print(len(post_listings))
+    #post_title=post_listings[0].find(class_='result-title').text
+    #post_url=post_listings[0].find('a').get('href')
+    #post_price=post_listings[0].find(class_='result-price').text
+    final_postings=[]
+    for post in post_listings:
+        post_title=post.find(class_='result-title').text
+        post_url=post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price=post.find(class_='result-price').text
+        else:
+            post_price='N/A'
+        final_postings.append((post_title,post_url,post_price))
 
-    print(post_title)
-    print(post_url)
-    print(post_price)
+    #print(post_title)
+    #print(post_url)
+    #print(post_price)
 
     #post_titles=soup.find_all('a',{'class':'result-title'})
     #print(post_titles[0].text)
@@ -37,6 +46,7 @@ def new_search(request):
     #print(data)
     #print(search)
     stuff_for_frontend={
-        'search':search
+        'search':search,
+        'final_postings':final_postings,
     }
     return render(request,'webscrapping/newsearch.html',stuff_for_frontend)
