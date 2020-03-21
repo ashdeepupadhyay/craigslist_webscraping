19d648a923507554fb9538aa77ea38d7d2ffd663
ashdeep <ashdeepupadhyay@gmail.com>
2020-03-21 15:03:47 +0530

    made the project working on windows laptop

---
+++ env_craigslist/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!G:\projects\python\django\craigslist_webscraping\env_craigslist\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
